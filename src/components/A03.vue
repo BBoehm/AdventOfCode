f1bfde4b2d3b5ca760a56c9380ca1d4d73d38d31
Benedikt Böhm <benedikt.boehm@diva-e.com>
2024-12-07 12:48:00 +0100

    add A03

---
+++ src/components/A03.vue
@@ -0,0 +1,28 @@
+<template>
+  Multiplication sum: {{sumOfMultiplication.toLocaleString()}}
+  <br/>
+  Cleaned multiplication sum: {{sumOfCleanedMultiplication.toLocaleString()}}
+</template>
+
+<script setup lang="ts">
+import input from './A03.input';
+
+const matchRegex = /mul\(\d{1,3},\d{1,3}\)/gm;
+const matches = input.match(matchRegex)!;
+
+// noinspection JSUnusedGlobalSymbols
+const mul = (a: number, b: number) => a * b;
+
+const sumOfMultiplication = matches.reduce((sum: number, match: string) => {
+  return sum + eval(match);
+}, 0);
+
+// -----
+
+const cleanedInput = input.replaceAll('\n', '').replaceAll(/don't\(\)(.*?)do\(\)/gm, '').replaceAll(/don't\(\)(.*)/gm, '');
+const cleanedMatches = cleanedInput.match(matchRegex)!;
+
+const sumOfCleanedMultiplication = cleanedMatches.reduce((sum: number, match: string) => {
+  return sum + eval(match);
+}, 0);
+</script>
